7fcc8cd5a1c5618c6f127fcef9ccaca8d2969da2
Zadie <zkruglov@icloud.com>
2022-08-03 13:27:47 -0700

    Added pictures, changed around the layout a bit

--- finalKWK2022Project/ViewController.swift
+++ finalKWK2022Project/ViewController.swift
@@ -13,7 +13,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var isabelleArsenault = [
+      "name" : "Isabelle Arsenault",
+      "instagram" : "@isabelle_arsenault"
+    ]
+    
+    var ramonaPintea = [
+        "name" : "Ramona Pintea",
+        "website" : "https://ramonapintea.com/"
+    ]
+    
+    var connyLehman = [
+        "name" : "Conny Lehman",
+        "instagram" : "@connylehmanart"
+    ]
+    
+    var monicaCook = [
+        "name" : "Monica Cook",
+        "instagram" : "@monicacookstudio"
+    ]
+    
+    var doaaEladl = [
+        "name" : "Doaa Eladl",
+        "instagram" : "@doaa.eladl.cartoon"
+    ]
+    
+    var laurieSimmons = [
+        "name" : "Laurie Simmons"
+    ]
+    
+    var mayaAngelou = [
+        "name" : "Maya Angelou"
+    ]
+    
+    var aniaHoo = [
+        "name" : "Ania Hoo",
+        "instagram" : "@aniahoo"
+    ]
 
 }
 
---
+++ finalKWK2022Project/segueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  segueViewController.swift
+//  finalKWK2022Project
+//
+//  Created by scholar on 8/3/22.
+//
+
+import UIKit
+
+class segueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
